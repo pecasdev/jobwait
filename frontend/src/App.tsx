4f0064af1930c1db2283cd2cb173d3c580410121
Peter Albu <pecasdev@gmail.com>
2024-06-04 14:54:32 -0400

    Add tsconfig.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,42 @@
-import * as React from 'react';
-import { useState } from 'react';
-import './App.css';
+import * as React from "react";
+import { useState } from "react";
+import "./App.css";
+import PromptCollector, { PromptDefinition } from "./reusable/PromptCollector";
 
-async function queryAddress(address) {
-  if (address){
-    const response = await fetch(address,
-      {method: 'GET',
-       headers: {'Content-Type': 'application/text'}
-      }
-    )
-    return response.text();
-  }
+async function queryAddress(address: string): Promise<string> {
+    if (address) {
+        const response = await fetch(address, {
+            method: "GET",
+            headers: { "Content-Type": "application/text" },
+        });
+        return response.text();
+    }
+
+    return Promise.reject("address not provided");
 }
 
-function handleSubmit(e, setResponse) {
-  // Prevent the browser from reloading the page
-  e.preventDefault();
+// why is typescript so limited, this should be a default function
+function formDataToEntries(formData: FormData): { [key: string]: any } {
+    const stuff: { [key: string]: any } = {};
+    formData.forEach((value, key) => (stuff[key] = value));
+    return stuff;
+}
 
-  // Read the form data
-  const form = e.target;
-  const formData = new FormData(form);
+function handleSubmit(e: React.FormEvent, setResponse: (_: string) => void) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
 
-  let formJson = Object.fromEntries(formData.entries());
-  queryAddress(formJson["queryInput"]).then((response) => setResponse(response))
-  // setResponse()
+    // Read the form data
+    const form: HTMLFormElement = e.target as HTMLFormElement; // remove "as"
+    const formData = new FormData(form);
 
-  // // You can pass formData as a fetch body directly:
-  // fetch('/some-api', { method: form.method, body: formData });
+    let formJson = formDataToEntries(formData);
+    queryAddress(formJson["queryInput"]).then((response) =>
+        setResponse(response ?? "some kind of error"),
+    );
+
+    // // You can pass formData as a fetch body directly:
+    // fetch('/some-api', { method: form.method, body: formData });
 }
 
 export default function App() {
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from "react";
 import './index.css';
-import App from './App.tsx';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createRoot} from 'react-dom/client'
 
 const rootElement = document.getElementById('root')
 
 if (rootElement){
-  const root = ReactDOM.createRoot(rootElement);
+  const root = createRoot(rootElement);
   root.render(
     <React.StrictMode>
       <App />
