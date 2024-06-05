547ae9f0a32aaed22663acf120dfea2c41ab99b4
Peter Albu <pecasdev@gmail.com>
2024-06-05 17:35:23 -0400

    Add java formatter config

--- backend/src/main/java/com/jobwait/GreetingController.java
+++ backend/src/main/java/com/jobwait/GreetingController.java
@@ -15,20 +15,19 @@ import com.jobwait.db.PostgreSQLExample;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @CrossOrigin("http://localhost:3000")
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	//#2 
-	//Use created env configuration class to pull env vars from application.yml
+	// #2
+	// Use created env configuration class to pull env vars from application.yml
 	@Autowired
 	private DbConfiguration dbconfig;
 
-	//#1
-	//PULL VALUES STRAIGHT FROM application.yml or Spring env
+	// #1
+	// PULL VALUES STRAIGHT FROM application.yml or Spring env
 	@Value("${spring.datasource.url}")
 	private String dbUrl;
 
@@ -43,27 +42,27 @@ public class GreetingController {
 		return dbUrl;
 	}
 
-	//Me being a little stupid LOL
+	// Me being a little stupid LOL
 	@PostMapping("/dbTest")
 	public String postMethodName(@RequestBody String entity) {
-		//TODO: process POST request
-		
+		// TODO: process POST request
+
 		return getDbEnvironmentVars();
 		// return attemptDbConnection();
 	}
-	
+
 	public static String attemptDbConnection() {
 		try {
-			return String.valueOf(PostgreSQLExample.dbTest("jdbc:postgresql://localhost:5432/mydatabase", 
-			"postgres", 
-			"password"));
+			return String.valueOf(
+					PostgreSQLExample.dbTest("jdbc:postgresql://localhost:5432/mydatabase",
+							"postgres",
+							"password"));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			return e.toString();
 		}
 	}
 
-	
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
