86fa8c5b4763db618c3e197dc635c95eca01eacb
Peter Albu <pecasdev@gmail.com>
2024-06-05 20:48:56 -0400

    Add debounced loading icon to Prompt
    See #6 for details on why we need this

--- frontend/src/reusable/Prompt.tsx
+++ frontend/src/reusable/Prompt.tsx
@@ -1,24 +1,67 @@
 import * as React from "react";
 import { useState } from "react";
 import { PromptDefinition } from "./PromptCollector";
+import debounceFunction from "../util/debounceFunction";
+import { LoadingIcon } from "./LoadingIcon";
 
-export default function Prompt(promptDefinition: PromptDefinition) {
-    function changeResponse(text: String) {
-        console.log(`response for "${promptDefinition.idKey}" changed to "${text}`)
+type PromptState = { showIcon: boolean; showLoading: boolean };
+
+export default class Prompt extends React.Component<
+    PromptDefinition,
+    PromptState
+> {
+    debounced: (...args: any[]) => void;
+
+    constructor(private promptDefinition: PromptDefinition) {
+        super(promptDefinition);
+
+        this.state = {
+            showIcon: false,
+            showLoading: false,
+        };
+        
+        this.debounced = debounceFunction((response: string) => {
+            this.changeResponse(response);
+            this.setState({ showLoading: false });
+        }, 2000);
     }
 
-    const [response, setResponse] = useState("");
-    return (
-        <div>
-            <label className="flex flex-col">
-                {promptDefinition.displayText}
-                <input
-                    className="border border-black border-4"
-                    id={promptDefinition.idKey}
-                    name="displayText"
-                    onChange={(e) => changeResponse(e.target.value)}
-                />
-            </label>
-        </div>
-    );
+    changeResponse(text: String) {
+        // put some "send to backend" code here
+        console.log(
+            `response for "${this.promptDefinition.idKey}" changed to "${text}"`,
+        );
+    }
+
+    doStuff(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            showIcon: true,
+            showLoading: true,
+        });
+        this.debounced(event.target.value);
+    }
+
+    render() {
+        return (
+            <div>
+                <label className="flex flex-col">
+                    {this.promptDefinition.displayText}
+
+                    <div className="flex flex-row">
+                        <input
+                            className="border border-black border-4"
+                            id={this.promptDefinition.idKey}
+                            name="displayText"
+                            onChange={this.doStuff.bind(this)}
+                        />
+                        <LoadingIcon
+                            key={this.state.showIcon}
+                            showIcon={this.state.showIcon}
+                            showLoading={this.state.showLoading}
+                        />
+                    </div>
+                </label>
+            </div>
+        );
+    }
 }
