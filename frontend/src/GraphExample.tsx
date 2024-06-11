73dda89bff39daaa39c941929fb4dadd97de9aec
Peter Albu <pecasdev@gmail.com>
2024-06-11 09:04:56 -0400

    Improve GraphExample

--- frontend/src/GraphExample.tsx
+++ frontend/src/GraphExample.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Line } from "react-chartjs-2";
+import LineChart from "./LineChart";
 
 const labels = ["3 days ago", "2 days ago", "1 day ago", "today", "tomorrow"];
 const Data = [100, 90, 85, 87, 77, 60, 67, 45, 32];
@@ -7,19 +6,13 @@ const Data = [100, 90, 85, 87, 77, 60, 67, 45, 32];
 export default function GraphExample() {
     const chartData = {
         labels,
-        datasets: [{ label: "🤪", data: Data }],
+        datasets: [{ label: "something", data: Data }],
+    };
+    const chartOptions = {
+        plugins: {
+            title: { display: true, text: "some graph" },
+        },
     };
 
-    return (
-        <span className="max-h-80 max-w-96">
-            <Line
-                data={chartData}
-                options={{
-                    plugins: {
-                        title: { display: true, text: "peter's will to live" },
-                    },
-                }}
-            />
-        </span>
-    );
+    return LineChart(chartData, chartOptions);
 }
