8a723f598d692cd520490ca532789b1ab97413e9
danielperev <daniel.perevalov@gmail.com>
2024-06-24 11:31:18 -0400

    Switch from headlessui to mantine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
-import * as React from "react";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+
 import "./App.css";
 import PromptCollector from "./reusable/PromptCollector";
 import { Chart, registerables } from "chart.js";
 import { defaultPromptDefinitions } from "./reusable/default/DefaultPromptDefinitions";
+import { MantineProvider } from "@mantine/core";
 
 Chart.register(...registerables);
 
 export default function App() {
     return (
-        <div>
-            <h1>job! wait...</h1>
-            <PromptCollector
-                promptDefinitions={defaultPromptDefinitions}
-            ></PromptCollector>
-        </div>
+        <MantineProvider>
+            <div>
+                <h1>job! wait...</h1>
+                <PromptCollector
+                    promptDefinitions={defaultPromptDefinitions}
+                ></PromptCollector>
+            </div>
+        </MantineProvider>
     );
 }
