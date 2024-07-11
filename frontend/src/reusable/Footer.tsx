f16d928f4fcb39b241bf5935c23b152241109c17
Peter Albu <pecasdev@gmail.com>
2024-07-11 16:06:39 -0400

    Refactor Footer

--- frontend/src/reusable/Footer.tsx
+++ frontend/src/reusable/Footer.tsx
@@ -1,16 +1,25 @@
+function peterContact() {
+    return (
+        <a href="https://github.com/pecasdev" target="_blank">
+            peter
+        </a>
+    );
+}
+
+function danielContact() {
+    return (
+        <a href="https://github.com/danielperev" target="_blank">
+            daniel
+        </a>
+    );
+}
+
 export function Footer() {
     return (
         <footer className="min-h-100%">
             <span className="absolute bottom-0">
                 <p className="h4">
-                    give us jobs:{" "}
-                    <a href="https://github.com/pecasdev" target="_blank">
-                        peter
-                    </a>
-                    ,{" "}
-                    <a href="https://github.com/danielperev" target="_blank">
-                        daniel
-                    </a>
+                    give us jobs: {peterContact()}, {danielContact()}
                 </p>
             </span>
         </footer>
