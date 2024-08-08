34032804a5e77748fcfed2030314215718247246
Peter Albu <pecasdev@gmail.com>
2024-08-08 13:26:58 -0400

    Add database faults to getUserFromAuthId

---
+++ backend/src/main/java/com/jobwait/persistence/PostgresControllerFaults.java
@@ -0,0 +1,22 @@
+package com.jobwait.persistence;
+
+import org.springframework.http.HttpStatus;
+
+import com.jobwait.fault.FaultException;
+
+public class PostgresControllerFaults {
+    public static FaultException UserNotFoundFault(String authId) {
+        return new FaultException(HttpStatus.NOT_FOUND, "USER_NOT_FOUND",
+                "Could not find user with authId: %s".formatted(authId));
+    }
+
+    public static FaultException GenericDatabaseFault() {
+        return new FaultException(HttpStatus.INTERNAL_SERVER_ERROR, "DATABASE_FAULT",
+                "Something unexpected happened in the database, check the logs for details");
+    }
+
+    public static FaultException DatabaseGetConnectionFault() {
+        return new FaultException(HttpStatus.INTERNAL_SERVER_ERROR, "DATABASE_INIT_FAULT",
+                "Could not get connection to database. Is it running?");
+    }
+}
