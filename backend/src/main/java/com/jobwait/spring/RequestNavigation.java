f4de5681e8aab95098762f7df7b569c887440151
Peter Albu <pecasdev@gmail.com>
2024-08-07 21:09:19 -0400

    Rewrite jackson deserializer

--- backend/src/main/java/com/jobwait/spring/RequestNavigation.java
+++ backend/src/main/java/com/jobwait/spring/RequestNavigation.java
@@ -1,5 +1,8 @@
 package com.jobwait.spring;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,13 +12,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.jobwait.control.RequestController;
-import com.jobwait.domain.Answers;
+import com.jobwait.domain.Answer;
 import com.jobwait.domain.User;
 import com.jobwait.security.AuthToken;
 import com.jobwait.spring.utils.Utils;
 
+// todo - remove all try/catch blocks from jobwait, there should be a single try/catch at the end or whenever we want to convert a runtimeexception/sqlexception into a more generic Fault exception
 @CrossOrigin("http://127.0.0.1:3000")
 @RestController
 public class RequestNavigation {
@@ -54,10 +60,12 @@ public class RequestNavigation {
 	public static ResponseEntity<String> submitUserAnswers(@RequestParam("at") String authToken,
 			@RequestBody String payload) {
 		try {
-			Answers answers = Utils.mapper.readValue(payload, Answers.class);
+			JsonNode root = Utils.mapper.readTree(payload).path("answers");
+			Answer[] answersArray =Utils.mapper.treeToValue(root,Answer[].class);
+			List<Answer> answersList = Arrays.asList(answersArray);
 
 			AuthToken token = AuthToken.fromClientId(authToken);
-			requestController.submitUserAnswers(token, answers);
+			requestController.submitUserAnswers(token, answersList);
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (JsonProcessingException | RuntimeException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
