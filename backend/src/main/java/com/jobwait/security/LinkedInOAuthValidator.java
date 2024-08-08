68036c20fb7dead72663df411704a0f08fc713cf
Peter Albu <pecasdev@gmail.com>
2024-08-08 13:27:10 -0400

    Add fault to LinkedInOAuthValidator

--- backend/src/main/java/com/jobwait/security/LinkedInOAuthValidator.java
+++ backend/src/main/java/com/jobwait/security/LinkedInOAuthValidator.java
@@ -1,11 +1,22 @@
 package com.jobwait.security;
 
+import org.springframework.http.HttpStatus;
+
+import com.jobwait.fault.FaultException;
+
 public class LinkedInOAuthValidator extends OAuthValidator {
     @Override
-    public void validateToken(AuthToken token) {
+    public void validateToken(AuthToken token) throws FaultException {
         if (!token.clientId().startsWith("valid")) {
-            throw new RuntimeException("authtoken doesn't start with the word 'valid', therefore invalid");
+            throw new InvalidTokenFault();
         }
         // todo - query linkedin oauth endpoint and check the token
+    }
+
+    private class InvalidTokenFault extends FaultException {
+        public InvalidTokenFault() {
+            super(HttpStatus.UNAUTHORIZED, "INVALID_AUTH_TOKEN",
+                    "authtoken doesn't start with the word 'valid', therefore invalid");
+        }
     }
 }
