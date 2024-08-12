bb85416b338c16f86a70c4abc9607aed3770801f
Peter Albu <pecasdev@gmail.com>
2024-08-12 19:32:24 -0400

    Implement Stat cache + refresh system

---
+++ backend/src/main/java/com/jobwait/stat/StatRefreshOfficer.java
@@ -0,0 +1,33 @@
+package com.jobwait.stat;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.jobwait.domain.Stat;
+
+public class StatRefreshOfficer {
+    // constants
+    private static Duration statLifetime = Duration.ofMinutes(60);
+
+    // stat id to timestamp
+    private static Map<String, Instant> generateIdStampMap() {
+        return Stats.known.stream().map(s -> s.id)
+                .collect(Collectors.toMap(Function.identity(), _ -> Instant.now(), (_, _) -> {
+                    throw new RuntimeException("impossible error");
+                }));
+    }
+
+    private Map<String, Instant> statIdToLastRefreshStamp = generateIdStampMap();
+
+    public void refreshStatIfNeeded(String statId) {
+        Instant lastRefreshStamp = this.statIdToLastRefreshStamp.get(statId);
+
+        if (Instant.now().isAfter(lastRefreshStamp.plus(statLifetime))) {
+            Stat stat = Stats.statFromId(statId);
+            stat.fetchAndSetRows();
+        }
+    }
+}
