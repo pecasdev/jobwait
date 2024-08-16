340458c31c5cd5dc8566c3386436e26bc8fcde82
Peter Albu <pecasdev@gmail.com>
2024-08-16 15:35:17 -0400

    Add key sorting to fetchStat

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,5 @@
 import { StatMold, StatShape } from "./shape/StatShape";
+import _ from "lodash";
 
 export default class Api {
     static backendUrl = "http://127.0.0.1:9000";
@@ -7,6 +8,14 @@ export default class Api {
         return fetch(`${Api.backendUrl}/stat?id=${statId}`)
             .then((resp) => resp.json())
             .then((json) => json["data"])
-            .then((data) => new StatMold().assert(data));
+            .then((data) => new StatMold().assert(data))
+            .then((stat) => {
+                // sort row keys
+                stat.rows = _.pick(
+                    stat.rows,
+                    Object.keys(stat.rows).sort(),
+                );
+                return stat;
+            });
     }
 }
