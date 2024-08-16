18199f307abff34fea47a51e90b8ae9951bf164a
Peter Albu <pecasdev@gmail.com>
2024-08-16 15:39:41 -0400

    Further abstract StatRowsShape

--- frontend/src/shape/BarStatShape.ts
+++ frontend/src/shape/BarStatShape.ts
@@ -1,33 +1,15 @@
-import { assertEqual, assertType, Shape } from "./Shape";
-import { StatMold } from "./StatShape";
+import { assertEqual, Shape } from "./Shape";
+import { StatMold, StatShape } from "./StatShape";
 
-export type BarStatRowShape = {
-    [_: string]: number;
-};
-export type BarStatShape = {
+export type BarStatShape = StatShape & {
     type: "bar";
-    rows: BarStatRowShape;
 };
-
-class BarStatRowMold extends Shape<BarStatRowShape> {
-    public assert(obj: any): BarStatRowShape {
-        return super.assert(obj);
-    }
-
-    protected override throwIfMisfit(obj: any) {
-        for (const key of Object.keys(obj)) {
-            assertType(obj[key], "number");
-        }
-    }
-}
 
 export class BarStatMold extends Shape<BarStatShape> {
     statMold = new StatMold();
-    barStatRowMold = new BarStatRowMold();
 
     protected override throwIfMisfit(obj: any) {
         this.statMold.assert(obj);
         assertEqual(obj["type"], "bar");
-        this.barStatRowMold.assert(obj["rows"]);
     }
 }
--- frontend/src/shape/StatShape.ts
+++ frontend/src/shape/StatShape.ts
@@ -1,16 +1,38 @@
+import { ArrayElement } from "../magic/ArrayElement";
 import { assertElementOf, assertKeyInShape, assertType, Shape } from "./Shape";
 
 const StatShapeTypes = ["bar", "line"];
-export type StatRowShape = { [_: string]: any };
+export type StatRowsShape = {
+    [key: string]: number | number[];
+};
 export type StatShape = {
-    type: (typeof StatShapeTypes)[number];
-    rows: StatRowShape;
+    type: ArrayElement<typeof StatShapeTypes>;
+    rows: StatRowsShape;
 };
 
+class StatRowMold extends Shape<StatRowsShape> {
+    public assert(obj: any): StatRowsShape {
+        return super.assert(obj);
+    }
+
+    protected override throwIfMisfit(obj: any) {
+        for (const key of Object.keys(obj)) {
+            if (Array.isArray(obj[key])) {
+                obj[key].every((x) => assertType(x, "number"));
+            } else {
+                assertType(obj[key], "number");
+            }
+        }
+    }
+}
+
 export class StatMold extends Shape<StatShape> {
+    statRowMold = new StatRowMold();
+
     protected override throwIfMisfit(obj: any) {
         assertKeyInShape(obj, "type");
         assertElementOf(obj["type"], StatShapeTypes);
         assertKeyInShape(obj, "rows");
+        this.statRowMold.assert(obj["rows"]);
     }
 }
