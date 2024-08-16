25a6a73aff5c1fa67b8fac00716ea6b658c6f759
Peter Albu <pecasdev@gmail.com>
2024-08-16 11:50:40 -0400

    Remove height+width from LineChart

--- frontend/src/components/stats/chart/LineChart.tsx
+++ frontend/src/components/stats/chart/LineChart.tsx
@@ -4,8 +4,6 @@ import { Line } from "react-chartjs-2";
 export default function LineChart(
     data: ChartData<"line", number[], string>,
     options: ChartOptions<"line">,
-    width: number,
-    height: number,
 ) {
-    return <Line data={data} options={options} width={width} height={height} />;
+    return <Line data={data} options={options} width={200} height={200} />;
 }
