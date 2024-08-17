9af82bd9af508d3719f6de382ed6e5a64a02a19a
danielperev <daniel.perevalov@gmail.com>
2024-08-17 16:08:59 -0400

    Implement foundation for LinkedInOAuthExchange class

---
+++ backend/src/main/java/com/jobwait/security/LinkedInOAuthExchange.java
@@ -0,0 +1,9 @@
+package com.jobwait.security;
+
+public class LinkedInOAuthExchange {
+
+    public void codeToToken(String authCode) {
+
+        // todo - query linkedin accessToken endpoint and exchange code for Bearer token
+    }
+}
