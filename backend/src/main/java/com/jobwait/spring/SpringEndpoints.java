6d72b6deeaad9c8f666e835d8b186e87dc80654f
Peter Albu <pecasdev@gmail.com>
2024-09-05 18:41:09 -0400

    Add frontend prod url to backend CORS

--- backend/src/main/java/com/jobwait/spring/SpringEndpoints.java
+++ backend/src/main/java/com/jobwait/spring/SpringEndpoints.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.jobwait.fault.FaultException;
 import com.jobwait.spring.utils.Utils;
 
-@CrossOrigin(origins={"http://127.0.0.1:3000", "http://localhost:3000"})
+@CrossOrigin(origins={"http://127.0.0.1:3000", "http://localhost:3000", "https://jobwait.pecas.dev"})
 @RestController
 public class SpringEndpoints {
 	private static ResponseEntity<ObjectNode> processAndHandleException(Supplier<ObjectNode> func) {
