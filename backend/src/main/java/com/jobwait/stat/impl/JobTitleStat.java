1668158db9ce888ef644e27c7a2796615064b7ad
Peter Albu <pecasdev@gmail.com>
2024-09-11 13:05:22 -0400

    Refactor Stat rows

--- backend/src/main/java/com/jobwait/stat/impl/JobTitleStat.java
+++ backend/src/main/java/com/jobwait/stat/impl/JobTitleStat.java
@@ -1,29 +1,19 @@
 package com.jobwait.stat.impl;
 
-import java.util.HashMap;
 import java.util.List;
 
 import com.jobwait.domain.Answer;
 import com.jobwait.domain.Stat;
 import com.jobwait.stat.AnswerFetching;
+import com.jobwait.stat.StatRows;
 
 public class JobTitleStat extends Stat implements AnswerFetching {
     public JobTitleStat(String id) {
         super(id, "bar", "Job Title", "Title", "# of people");
     }
 
-    // todo - put this into a defaultdict util class
-    private void addOrIncrementRow(String title) {
-        if (this.rows.containsKey(title)) {
-            int prev = (int) this.rows.get(title);
-            this.rows.put(title, prev + 1);
-        } else {
-            this.rows.put(title, 1);
-        }
-    }
-
     public void fetchAndSetRows() {
-        this.rows = new HashMap<String, Object>();
+        this.rows = new StatRows();
 
         List<List<Answer>> answers = this.fetchAllAnswerSets();
         for (List<Answer> answerSet : answers) {
@@ -31,7 +21,7 @@ public class JobTitleStat extends Stat implements AnswerFetching {
                 if (answer.questionKey.equals("jobtitle")) {
                     String title = answer.answerValueAsString();
                     if (title != null) {
-                        this.addOrIncrementRow(answer.answerValueAsString());
+                        this.rows.addOrIncrementRow(answer.answerValueAsString());
                     }
                 }
             }
