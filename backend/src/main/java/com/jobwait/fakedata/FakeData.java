d65a93ab4d618733ff680ed8feac3fbc7f4d7d40
Peter Albu <pecasdev@gmail.com>
2024-09-12 15:38:11 -0400

    Multithread FakeData
    I also added more relations to the data to help show more interesting things on the graphs

--- backend/src/main/java/com/jobwait/fakedata/FakeData.java
+++ backend/src/main/java/com/jobwait/fakedata/FakeData.java
@@ -2,10 +2,18 @@ package com.jobwait.fakedata;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -18,24 +26,31 @@ import com.jobwait.persistence.PostgresController;
 public class FakeData {
     PostgresController postgresController = new PostgresController();
 
-    int NUMBER_OF_FAKE_USERS = 100;
+    int NUMBER_OF_FAKE_USERS = 1000;
 
-    private List<User> createFakeUsers() {
-        List<User> users = new ArrayList<User>();
-
-        for (int i = 0; i != NUMBER_OF_FAKE_USERS; i++) {
-            User user = postgresController.createUserFromAuthId("FAKE_".concat(UUID.randomUUID().toString()));
-            users.add(user);
-        }
-
-        return users;
+    private User createFakeUser() {
+        User user = postgresController.createUserFromAuthId("FAKE_".concat(UUID.randomUUID().toString()));
+        return user;
     }
 
-    int JSSD_DAYS_AGO = 365;
+    int JSSD_DAYS_AGO = 365 * 3;
     int JAD_DAYS_AFTER_START_MIN = 5;
-    int JAD_DAYS_AFTER_START_MAX = 365;
+    int JAD_DAYS_AFTER_START_MAX = 1000;
     int JAD_DAYS_AFTER_START_AVG = 100;
     int JAD_DAYS_AFTER_START_STD = 30;
+    Map<String, Integer> JAD_DAYS_AFTER_START_EDUCATION_MODIFIERS = Map.of(
+            "HIGHSCHOOL_DIPLOMA", 3,
+            "BACHELOR_DEGREE", 2,
+            "MASTER_DEGREE", 1,
+            "DOCTORAL_DEGREE", 1);
+    Map<Integer, Integer> JAD_DAYS_AFTER_START_YOE_MODIFIERS = Map.of(
+            0, 4,
+            1, 3,
+            2, 3,
+            3, 2,
+            4, 2,
+            5, 1,
+            6, 1);
     List<String> WM_WEIGHTED_OPTIONS = Stream.of(
             Collections.nCopies(20, "ON_SITE").stream(),
             Collections.nCopies(10, "HYBRID").stream(),
@@ -46,8 +61,9 @@ public class FakeData {
             Collections.nCopies(5, "CONTRACT").stream(),
             Collections.nCopies(2, "INTERNSHIP").stream(),
             Collections.nCopies(1, "OTHER").stream()).flatMap(Function.identity()).toList();
-    int JAC_MIN = 10;
-    int JAC_MAX = 5000;
+    int JAC_MIN = 50;
+    int JAC_MAX = 100000;
+    int JAC_STD = 250;
     List<String> JT_WEIGHTED_OPTIONS = Stream.of(
             Collections.nCopies(20, "Fullstack Developer").stream(),
             Collections.nCopies(10, "Frontend Developer").stream(),
@@ -64,32 +80,8 @@ public class FakeData {
             Collections.nCopies(1, "DOCTORAL_DEGREE").stream()).flatMap(Function.identity()).toList();
 
     private void createFakeAnswersForUser(User user) {
-        // jobsearchstartdate - everyone starts on the same day
-        LocalDate jobSearchStartDate = LocalDate.now().minusDays(JSSD_DAYS_AGO);
-
-        // jobacceptdate
-        LocalDate jobAcceptDate = null;
-        while (true) {
-            jobAcceptDate = jobSearchStartDate
-                    .plusDays(NormalRandom.sampleInt(JAD_DAYS_AFTER_START_AVG, JAD_DAYS_AFTER_START_STD));
-            long daysAfterStart = ChronoUnit.DAYS.between(jobSearchStartDate, jobAcceptDate);
-
-            if ((daysAfterStart >= JAD_DAYS_AFTER_START_MIN) && (daysAfterStart <= JAD_DAYS_AFTER_START_MAX)) {
-                break;
-            }
-        }
-
-        // workmodel
-        String workModel = ListRandom.choose(WM_WEIGHTED_OPTIONS);
-
-        // workcontract
-        String workContract = ListRandom.choose(WC_WEIGHTED_OPTIONS);
-
-        // jobapplicationcount
-        int jobApplicationCount = IntRandom.sampleInt(JAC_MIN, JAC_MAX);
-
-        // jobtitle
-        String jobTitle = ListRandom.choose(JT_WEIGHTED_OPTIONS);
+        // educationlevel
+        String educationLevel = ListRandom.choose(EL_WEIGHTED_OPTIONS);
 
         // years of proexperience
         int yearsOfProExperience = -1;
@@ -100,8 +92,49 @@ public class FakeData {
             }
         }
 
-        // educationlevel
-        String educationLevel = ListRandom.choose(EL_WEIGHTED_OPTIONS);
+        // jobsearchstartdate - everyone starts on the same day
+        LocalDate jobSearchStartDate = LocalDate.now().minusDays(JSSD_DAYS_AGO);
+
+        // jobacceptdate
+        LocalDate jobAcceptDate = null;
+        int jobAcceptDateDaysAfterSearchStartDate;
+
+        int jadDaysAfterStartAverageWithEducation = JAD_DAYS_AFTER_START_AVG
+                * JAD_DAYS_AFTER_START_EDUCATION_MODIFIERS.get(educationLevel);
+        int jadDaysAfterStartAverageWithEducationAndYearsOfExperience = jadDaysAfterStartAverageWithEducation
+                * JAD_DAYS_AFTER_START_YOE_MODIFIERS.get(yearsOfProExperience);
+
+        while (true) {
+            jobAcceptDate = jobSearchStartDate
+                    .plusDays(NormalRandom.sampleInt(jadDaysAfterStartAverageWithEducationAndYearsOfExperience,
+                            JAD_DAYS_AFTER_START_STD));
+            jobAcceptDateDaysAfterSearchStartDate = (int) ChronoUnit.DAYS.between(jobSearchStartDate, jobAcceptDate);
+
+            if ((jobAcceptDateDaysAfterSearchStartDate >= JAD_DAYS_AFTER_START_MIN)
+                    && (jobAcceptDateDaysAfterSearchStartDate <= JAD_DAYS_AFTER_START_MAX)) {
+                break;
+            }
+        }
+
+        // workmodel
+        String workModel = ListRandom.choose(WM_WEIGHTED_OPTIONS);
+
+        // workcontract
+        String workContract = ListRandom.choose(WC_WEIGHTED_OPTIONS);
+
+        // jobapplicationcount - assume 10 applications per day to guarantee success (LOL)
+        int jobApplicationCount;
+        while (true) {
+            jobApplicationCount = NormalRandom.sampleInt(
+                    jobAcceptDateDaysAfterSearchStartDate * 10, JAC_STD);
+
+            if ((jobApplicationCount >= JAC_MIN) && (jobApplicationCount <= JAC_MAX)) {
+                break;
+            }
+        }
+
+        // jobtitle
+        String jobTitle = ListRandom.choose(JT_WEIGHTED_OPTIONS);
 
         // submit answers
         List<Answer> answers = List.of(
@@ -117,22 +150,67 @@ public class FakeData {
         postgresController.updateUserAnswers(user, answers);
     }
 
-    public void createFakeUsersAndAnswers() {
-        List<User> users = createFakeUsers();
-        System.out.println("Created %d users".formatted(users.size()));
-
-        System.out.println("Creating fake answers...");
-        for (User user : users) {
+    private Callable<Void> createFakeUsersAndAnswersHandle() {
+        return () -> {
+            User user = createFakeUser();
             createFakeAnswersForUser(user);
+            return null;
+        };
+    }
+
+    int INDIVIDUAL_FAKERY_TIMEOUT_SECONDS = 10;
+    int OVERALL_FAKERY_TIMEOUT_SECONDS = 60;
+
+    public void createFakeUsersAndAnswers() {
+        ExecutorService executor = Executors.newFixedThreadPool(100);
+
+        for (int i = 0; i != NUMBER_OF_FAKE_USERS; i++) {
+            executor.submit(() -> {
+                ExecutorService individual = Executors.newSingleThreadExecutor();
+
+                Future<Void> future = individual.submit(createFakeUsersAndAnswersHandle());
+
+                try {
+                    future.get(INDIVIDUAL_FAKERY_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                } catch (TimeoutException e) {
+                    System.err.println("1 task out of %d has timed out, skipping".formatted(NUMBER_OF_FAKE_USERS));
+                    future.cancel(true);
+                } catch (InterruptedException | ExecutionException e) {
+                    System.err.println("Task interrupted or failed");
+                    System.err.print(e);
+                    Thread.currentThread().interrupt();
+                }
+            });
         }
+
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(OVERALL_FAKERY_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+                System.out.println("Could not finish all tasks in time");
+
+                ThreadPoolExecutor tpe = (ThreadPoolExecutor) executor;
+                int completedCount = (int) tpe.getCompletedTaskCount();
+                System.out.println("Finished %d tasks before timing out".formatted(completedCount));
+
+                executor.shutdownNow();
+                if (!executor.awaitTermination(OVERALL_FAKERY_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminate");
+                }
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
     }
 
     public static void main(String[] args) {
         if (args.length > 0) {
             if (args[0].equals("createFakeData")) {
-                System.out.println("creating fake data...");
+                System.out.println("creating fake data... (should take under a minute)");
                 new FakeData().createFakeUsersAndAnswers();
-                System.out.println("done creating fake data");
+                System.out.println("done! cleaning up and exiting... (I sometimes hang for some reason, feel free to kill me)");
                 return;
             }
         }
